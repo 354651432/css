3eeb2adf2d5f97ed2ed79bfd08df3ac855e549ab
dual <dual@dr.h>
2021-10-13 11:56:18 +0800

    add sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
+import "./index.scss"
 
 const app = document.createElement("div")
 document.body.appendChild(app)
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     mode: "development",
+    stats: {
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    },
     module: {
         rules: [
             {
@@ -20,7 +26,18 @@ module.exports = {
                         ]
                     }
                 }]
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     },
     plugins: [new HtmlWebpackPlugin()],
